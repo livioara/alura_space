c474c592fd7b4754eb979c3ff0d5da18b1e8c78a
livioara <assislivio@gmail.com>
2023-12-31 13:39:19 -0300

    Curso01

--- galeria/views.py
+++ galeria/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
+def index(request):
+    return render(request,'index.html')
+
+def imagem(request):
+    return render(request,'imagem.html')
+
 # Create your views here.
